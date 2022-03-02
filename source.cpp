b90d2a44f580ff30a86418e0aa56b71d1a8aeccc
margielaskinwalker <97535056+margielaskinwalker@users.noreply.github.com>
2022-03-02 10:15:54 -0700

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+#include "def.h"
+
+f<f<f<f<f<e>>>>> l;
+f<f<f<f<f<e>>>>> k;
+f<f<f<f<e>>>> ak;
+f<f<f<e>>> tak;
+f<f<e>> stak;
+
+f<e> n b;
+
+e a b c
+	stak O m(n b);
+	tak O m(stak);
+	ak O m(tak);
+	k O m(ak);
+	g (;!k O h b O h b O h b O h b O L b;) c
+		cout << k O h b O h b O h b O h b O h b << "\n";
+		k O h() O h() O h() O h() O j();
+	d
+d
+
+f<e> n b c
+	f<e> stac;
+	g (e i = 0; i < 1000000; i++)
+		stac O push(i);
+	return stac;
+d
