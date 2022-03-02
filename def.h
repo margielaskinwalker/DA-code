e865a806c89ad98ebb982498a5feda5bea67587b
margielaskinwalker <97535056+margielaskinwalker@users.noreply.github.com>
2022-03-02 10:16:22 -0700

    Create def.h

---
+++ def.h
@@ -0,0 +1,13 @@
+#pragma once
+#define a main
+#define b ()
+#define c {
+#define d }
+#define e int
+#define f stack
+#define g for
+#define h top
+#define j pop
+#define L empty
+#define m push
+#define O  .
